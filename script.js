3b92ed77a87d1515adf43d7df751818dff73a4c3
code kazuki <agbowurotoyin8@gmail.com>
2023-04-21 14:20:25 +0100

    js started

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-'ues script'
+"ues script";
+
+const player0El = document.querySelector("#score--0");
+const player1El = document.querySelector("#score--1");
+const dice = document.querySelector(".dice");
+
+player0El.textContent = 0;
+player1El.textContent = 0;
+dice.classList.add("hidden");
