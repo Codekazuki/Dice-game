2aff434a659d1c9142f04cdb5334dcad999e9b80
code kazuki <agbowurotoyin8@gmail.com>
2023-04-22 22:41:05 +0100

    src vs scr

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 
 const player0El = document.querySelector("#score--0");
 const player1El = document.querySelector("#score--1");
-const dice = document.querySelector(".dice");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 player0El.textContent = 0;
 player1El.textContent = 0;
-dice.classList.add("hidden");
+diceEl.classList.add("hidden");
+
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+});
