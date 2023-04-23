6c6a7f521abc564c3fe3746d290992df72464f3b
code kazuki <agbowurotoyin8@gmail.com>
2023-04-23 13:11:59 +0100

    dice plus score

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const player0El = document.querySelector("#score--0");
 const player1El = document.querySelector("#score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -10,10 +12,19 @@ const btnHold = document.querySelector(".btn--hold");
 player0El.textContent = 0;
 player1El.textContent = 0;
 diceEl.classList.add("hidden");
+let currentScore = 0;
 
 btnRoll.addEventListener("click", function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
   console.log(dice);
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    currentScore += dice;
+    current1El.textContent = currentScore;
+  }
 });
