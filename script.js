8884e07b33f65a83edf516ed995c34ab347850e3
code kazuki <agbowurotoyin8@gmail.com>
2023-04-23 15:26:10 +0100

    90%

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "ues script";
 
-const player0El = document.querySelector("#score--0");
-const player1El = document.querySelector("#score--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
@@ -9,10 +13,21 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-player0El.textContent = 0;
-player1El.textContent = 0;
+score0El.textContent = 0;
+score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore = 0;
+  currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 btnRoll.addEventListener("click", function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -22,9 +37,26 @@ btnRoll.addEventListener("click", function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    currentScore += dice;
-    current1El.textContent = currentScore;
+    switchPlayer();
+  }
+});
+btnHold.addEventListener("click", function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 10) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
+  } else {
+    switchPlayer();
   }
 });
