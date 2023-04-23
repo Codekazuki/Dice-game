a36e16ac323a7dc6515708e9e74f74a67113cb04
code kazuki <agbowurotoyin8@gmail.com>
2023-04-23 21:37:13 +0100

    remaining mobile

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"ues script";
+"use script";
 
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
@@ -8,19 +8,32 @@ const score1El = document.querySelector("#score--1");
 
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent =
     currentScore = 0;
@@ -69,18 +82,4 @@ btnHold.addEventListener("click", function () {
   }
 });
 
-btnNew.addEventListener("click", function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  diceEl.classList.add("hidden");
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  player1El.classList.remove("player--winner");
-  player0El.classList.remove("player--winner");
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
-  playing = true;
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-});
+btnNew.addEventListener("click", init);
